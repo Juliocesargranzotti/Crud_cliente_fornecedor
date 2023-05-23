0cd2fc6e2a4b5c6c6134f3c8ea6e95eb10ad34d9
Juliocesargranzotti <jcgranzotti@gmail.com>
2023-05-23 17:55:08 -0300

    terminar fornecedor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FornecedorComponent } from './fornecedor/fornecedor.component';
 import { HomeComponent } from './home/home.component';
 import { SupplierComponent } from './supplier/supplier.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'supplier', component: SupplierComponent},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'fornecedor', component: FornecedorComponent}
 
 ];
 
